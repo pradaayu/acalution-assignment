152f9d4a4fc21303c196834035576e102ffdd584
pradaayu <pradaayu@gmail.com>
2021-04-22 23:36:39 +0700

    clean warnings

--- src/components/compability/modal.provider.js
+++ src/components/compability/modal.provider.js
@@ -32,4 +32,4 @@ export const ModalProvider = ({children}) => {
   )
 }
 
-export default ModalProvider;
+export default ModalProvider
--- src/components/productlist/product-list.hooks.js
+++ src/components/productlist/product-list.hooks.js
@@ -6,7 +6,6 @@ import { BASE_URL } from '../../constants'
 
 const useProductList = (page) => {
   const [productList, setProductList] = useState([])
-  const [totalPage, setTotalPage] = useState(0)
   const [visibleModalError, setVisibleModalError] = useState(false)
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(false)
@@ -16,35 +15,36 @@ const useProductList = (page) => {
   // console.log('product list', productList)
 
   useEffect(() => {
-    getProductList(page)
-  }, [page])
-  const getProductList = async (page) => {
-    setLoading(true)
-    setError(false)
-    try {
-      const params = {
-        page: page,
-      }
-      const res = await axios.get(`${BASE_URL}/list?page=${params.page}`)
-      if (res.status === 200) {
-        const products = res.data.value.products
-        setTotalPage(Math.ceil(res.data.value.totalCount / 20))
-        setProductList(prevProductList => {
-          return [...prevProductList, ...products]
-        })
-        setLoadMore(page !== totalPage) // check if there's still data to load
+    const getProductList = async (page) => {
+      setLoading(true)
+      setError(false)
+      try {
+        const params = {
+          page: page,
+        }
+        const res = await axios.get(`${BASE_URL}/list?page=${params.page}`)
+        if (res.status === 200) {
+          const products = res.data.value.products
+          const totalPage = Math.ceil(res.data.value.totalCount / 20)
+          setProductList(prevProductList => {
+            return [...prevProductList, ...products]
+          })
+          setLoadMore(page !== totalPage) // check if there's still data to load
+          setLoading(false)
+        }
+      } catch (error) {
+        setError(true)
+        setErrorMessage(error)
+        setVisibleModalError(true)
         setLoading(false)
       }
-    } catch (error) {
-      setError(true)
-      setErrorMessage(error)
-      setVisibleModalError(true)
-      setLoading(false)
     }
-  }
+    getProductList(page)
+  }, [page])
+
   return {
     productList,handleCloseError, visibleModalError,
-    errorMessage, loading, error, loadMore, totalPage
+    errorMessage, loading, error, loadMore
   }
 }
 
